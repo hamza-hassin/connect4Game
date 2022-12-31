afe5b0f26ef2feeda86213867de92c6542e248ca
hamza-hassin <hamza.hassine10000@gmail.com>
2022-12-31 17:17:29 +0100

    added settings

---
+++ setting.py
@@ -0,0 +1,14 @@
+import pygame
+
+Column1 = pygame.Rect(215, 220, 63, 415)
+Column2 = pygame.Rect(290, 220, 58, 415)
+Column3 = pygame.Rect(357, 220, 60, 415)
+Column4 = pygame.Rect(426, 220, 60, 415)
+Column5 = pygame.Rect(494, 220, 60, 415)
+Column6 = pygame.Rect(563, 220, 60, 415)
+Column7 = pygame.Rect(630, 220, 60, 415)
+
+icon = pygame.image.load('images/board-game.png')
+board_img = pygame.image.load('images/palett.png')
+redBall = pygame.image.load('images/redBal.png')
+blackBall = pygame.image.load('images/blackBal.png')
